97b0f351702bec8f502d4b0d9a9d892e78ec3fb9
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-05-25 09:46:17 -0400

    updated queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,59 +1,18 @@
-import { gql } from '@apollo/client';
+import {gql} from "@apollo/client";
 
 export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
+  query user($userId: ID!) {
+    user(_id: $userId) {
       _id
       username
-      email
-      thoughts {
+      savedBooks {
         _id
-        thoughtText
-        createdAt
-      }
-    }
-  }
-`;
-
-export const QUERY_THOUGHTS = gql`
-  query getThoughts {
-    thoughts {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-    }
-  }
-`;
-
-export const QUERY_SINGLE_THOUGHT = gql`
-  query getSingleThought($thoughtId: ID!) {
-    thought(thoughtId: $thoughtId) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        commentAuthor
-        createdAt
-      }
-    }
-  }
-`;
-
-export const QUERY_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
-        createdAt
+        authors
+        description
+        bookId
+        image
+        link
+        title
       }
     }
   }
